04d18c7288a6eb880d9eea56ae9b5b2e2bd04ca0
MSI -Jakub Turowski <turek979@gmail.com>
2023-09-26 08:43:43 +0200

    add pop up window

--- src/script.js
+++ src/script.js
@@ -13,9 +13,40 @@ typewriter = () => {
 
 window.addEventListener("load", typewriter);
 
+// TODO animater border
 window.addEventListener("load", function () {
     
     const border = document.querySelector(".myPhoto");
 
     border.style.transform = "rotate(360deg)";
+});
+
+//Pop up windows
+document.addEventListener('DOMContentLoaded', function () {
+    const modal = document.getElementById('myModal');
+    const openModalButton = document.getElementById('openModalButton');
+    const heroImage = document.getElementById('heroImage');
+    const modalContent = document.querySelector('.modal-content'); // Added this line
+
+    openModalButton.addEventListener('click', function () {
+        modal.classList.add('active');
+        heroImage.classList.add('hero');
+        modalContent.classList.add('active');
+    });
+
+    modal.addEventListener('click', function (event) {
+        if (event.target === modal || event.target.classList.contains('close')) {
+            modal.classList.remove('active');
+            heroImage.classList.remove('hero');
+            modalContent.classList.remove('active');
+        }
+    });
+
+    document.addEventListener('keydown', function (event) {
+        if (event.key === 'Escape' && modal.classList.contains('active')) {
+            modal.classList.remove('active');
+            heroImage.classList.remove('hero');
+            modalContent.classList.remove('active');
+        }
+    });
 });
